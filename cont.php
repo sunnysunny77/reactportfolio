4ffe745a6a856a54ea93f86f63305582cd87afb6
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2020-10-27 15:07:16 +0800

    final

---
+++ cont.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_REQUEST["name"];
+    $phone = $_REQUEST["phone"];
+    $email = $_REQUEST["email"];
+    $message = $_REQUEST["message"];
+    $to_email = "shlooby07@gmail.com";
+    $subject = "New Contact Us Message";
+    
+    $contactus = "
+    You have a message from the contact us page on your website:
+    Name: ".$name."
+    Phone: ".$phone."
+    Email: ".$email."
+    Message: ".$message;
+
+    $contactus  = wordwrap($contactus ,70);
+
+    mail($to_email,$subject,$contactus);
+}
+?>
+
+    <pre> Thanks, sent to &#128231; </pre> 
+
+</body>
+</html>
