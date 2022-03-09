b91bd62cecd623efad4f8a75fa775e6126c12e22
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-03-09 17:49:24 +0800

    up

--- js/sites.js
+++ js/sites.js
@@ -1,22 +1,14 @@
-const pictureID = [
-    "s51",
-    "s52",
-    "s53",
-    "s54",
-    "s55",
-    "s56",
-    "s57",
-    "s58",
-    "s59",
-    "s510",
-];
+let pictureID = [];
+
+let img = document.querySelectorAll(".sitesImg");
+for (let i = 1; i <= img.length; i++) {
+    pictureID.push("s5" + i)
+}
 
 function close(isnt) {
-    for (let x = 0; x < pictureID.length; x++) {
-        if (x > 0) {
-            const obj = document.getElementById(pictureID[x]);
-            obj.style.display = "none";
-        }
+    for (let x = 1; x < pictureID.length; x++) {
+        const obj = document.getElementById(pictureID[x]);
+        obj.style.display = "none";
     }
     document.getElementById(pictureID[0]).style.display = "block";
     isnt.scroll({ y: 0 });
@@ -47,49 +39,38 @@ function none(a) {
 }
 
 function range(event) {
+
+    let ranges = []
+
+    let preview = document.querySelectorAll(".scrollPreview");
+    for (let i = 0; i < preview.length - 1; i++) {
+        ranges.push(preview[i].offsetTop)
+    }
+    ranges.push(preview[preview.length - 1].offsetHeight)
+
+    let scroll_pos = event.scrollTop;
+
+    const rangesLenght = ranges.length;
+    const pictureLenght = pictureID.length;
+
     const a = event.scrollHeight;
     const b = document.getElementById("ob0").clientHeight;
-    const c = document.getElementById("wp").offsetTop;
-    const d = document.getElementById("pff").offsetTop;
-    const e = document.getElementById("acc").offsetTop;
-    const f = document.getElementById("stick").offsetTop;
-    const g = document.getElementById("bus").offsetTop;
-    const h = document.getElementById("sec").offsetTop;
-    const i = document.getElementById("cart").offsetTop;
-    const j = document.getElementById("store").offsetTop;
-    const k = document.getElementById("cand").offsetHeight;
-    let scroll_pos = event.scrollTop;
-    //a - b - last obj offsetHeight
-    const botCalc = a - b - k;
-    if (scroll_pos < c) {
+    const botCalc = a - b - ranges[rangesLenght - 1];
+
+    if (scroll_pos < ranges[0]) {
         none(pictureID[0]);
     }
-    if (scroll_pos > c && scroll_pos < d) {
-        none(pictureID[1]);
+    for (let i = 0; i < rangesLenght - 2; i++) {
+        let y = i + 1;
+        if (scroll_pos > ranges[i] && scroll_pos < ranges[y]) {
+            none(pictureID[y]);
+        }
     }
-    if (scroll_pos > d && scroll_pos < e) {
-        none(pictureID[2]);
-    }
-    if (scroll_pos > e && scroll_pos < f) {
-        none(pictureID[3]);
-    }
-    if (scroll_pos > f && scroll_pos < g) {
-        none(pictureID[4]);
-    }
-    if (scroll_pos > g && scroll_pos < h) {
-        none(pictureID[5]);
-    }
-    if (scroll_pos > h && scroll_pos < i) {
-        none(pictureID[6]);
-    }
-    if (scroll_pos > i && scroll_pos < j) {
-        none(pictureID[7]);
-    }
-    if (scroll_pos > j && scroll_pos < botCalc) {
-        none(pictureID[8]);
+    if (scroll_pos > ranges[rangesLenght - 2] && scroll_pos < botCalc) {
+        none(pictureID[pictureLenght - 2]);
     }
     if (scroll_pos > botCalc) {
-        none(pictureID[9]);
+        none(pictureID[pictureLenght - 1]);
     }
 }
 
