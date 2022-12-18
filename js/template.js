e00d2f9ce9b7afe744b425c5e1bfaf7be2f256e4
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-12-18 23:01:02 +0800

    Update template.js

--- js/template.js
+++ js/template.js
@@ -1,3 +1,20 @@
+function service() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('worker.js', { scope: '/' })
+      .then((registration) => {
+        const data = {
+          type: 'CACHE_URLS',
+          payload: [
+            location.href,
+            ...performance.getEntriesByType('resource').map((r) => r.name)
+          ]
+        };
+        registration.installing.postMessage(data);
+      })
+      .catch((err) => console.log('SW registration FAIL:', err));
+  }
+}
+
 function toggle() {
   document.getElementsByClassName("click")[0].classList.toggle("ani");
   document.getElementsByClassName("menu0")[0].classList.toggle("disp");
@@ -6,33 +23,18 @@ function toggle() {
 
 function vivus() {
   new Vivus(
-      "my-svg",
-      {
-          duration: 800,
-          file: "https://gendrive.s3-ap-southeast-2.amazonaws.com/svg.svg",
-      },
-      null
+    "my-svg",
+    {
+      duration: 800,
+      file: "https://gendrive.s3-ap-southeast-2.amazonaws.com/svg.svg",
+    },
+    null
   );
 }
 
 eventListner(window, "scroll", cache, null);
-eventListner(document.getElementsByClassName("click")[0],"click",toggle,false);
-eventListner(window, "load", () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('worker.js', { scope: '/' })
-        .then((registration) => {
-            const data = {
-                type: 'CACHE_URLS',
-                payload: [
-                    location.href,
-                    ...performance.getEntriesByType('resource').map((r) => r.name)
-                ]
-            };
-            registration.installing.postMessage(data);
-        })
-        .catch((err) => console.log('SW registration FAIL:', err));
-}
-}, null);
+eventListner(document.getElementsByClassName("click")[0], "click", toggle, false);
+eventListner(window, "load", service, null);
 
 window.onload = function () {
   script();
