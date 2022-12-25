175f9293ad0cfc460016554043b81662738c56e8
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-12-25 12:28:46 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -80,18 +80,23 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-
+  // Let the browser do its default thing
+  // for non-GET requests.
   if (event.request.method !== "GET") return;
-
-  event.respondWith(caches.open("v1").then((cache) => {
-
-    return fetch(event.request).then((fetchedResponse) => {
-      cache.add(event.request);
-
-      return fetchedResponse;
-    }).catch(() => {
-
-      return cache.match(event.request);
-    });
-  }));
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(
+    (async () => {
+      // Try to get the response from a cache.
+      const cache = await caches.open("v1");
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) {
+        // If we found a match in the cache, return it, but also
+        // update the entry in the cache in the background.
+        event.waitUntil(cache.add(event.request));
+        return cachedResponse;
+      }
+      // If we didn't find a match in the cache, use the network.
+      return fetch(event.request);
+    })()
+  );
 });
