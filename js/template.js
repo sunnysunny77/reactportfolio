3b4bb81052944609f9a32a272eb21cc8b66ec9f6
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-12-28 11:49:13 +0800

    ip

--- js/template.js
+++ js/template.js
@@ -1,7 +1,21 @@
 function toggle() {
-  document.getElementsByClassName("click")[0].classList.toggle("ani");
-  document.getElementsByClassName("menu0")[0].classList.toggle("disp");
-  document.getElementsByClassName("menu1")[0].classList.toggle("ani1");
+
+  const menu0 = document.getElementsByClassName("menu0")[0];
+  const click = document.getElementsByClassName("click")[0];
+
+  if (menu0.classList.contains("ani1")) {
+
+    menu0.classList.add("ani6")
+    click.classList.add("ani7");
+  } else if (menu0.classList.contains("ani6")) {
+
+    menu0.classList.remove("ani6")
+    click.classList.remove("ani7");
+  }
+  
+  click.classList.toggle("ani");
+  menu0.classList.toggle("disp");
+  menu0.classList.toggle("ani1")
 }
 
 function vivus() {
@@ -16,7 +30,7 @@ function vivus() {
 }
 
 eventListner(window, "scroll", cache, null);
-eventListner(document.getElementsByClassName("click")[0], "click", toggle, false);
+eventListner(document.querySelector("header"), "click", toggle, false);
 
 window.onload = function () {
   script();
