cf9c3167ab013c9da148a9c53f2c37daf04b1d0d
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-01-19 20:02:51 +0800

    up

--- js/overlay.js
+++ js/overlay.js
@@ -1,14 +1,14 @@
 function overlayInit() {
+    let instance = OverlayScrollbars(document.getElementById("ob0"), {
+        className: "os-theme-dark os-theme-dark-edgy",
+        callbacks: {
+            onScroll: cache,
+            onInitialized: eventListner(document.getElementById("close"), "click", function () {
+                instance.scroll({ y : "0%" }, 2500);
+            }, false)
+        },
+    });
     OverlayScrollbars(document.getElementById("overlay"), {
         className: "os-theme-dark os-theme-dark-edgy",
     });
-    if (document.getElementById("ob0")) {
-        let isnt = OverlayScrollbars(document.getElementById("ob0"), {
-            className: "os-theme-dark os-theme-dark-edgy",
-            callbacks: {
-                onScroll: cache,
-            },
-        });
-       
-    }
 }
