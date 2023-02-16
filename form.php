4c48a40744ebf0da73914dcfab98b84eac470619
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-02-16 13:45:02 +0800

    Update form.php

--- form.php
+++ form.php
@@ -10,15 +10,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_REQUEST["message"];
     $to_email = "shlooby07@gmail.com";
     $subject = "New Contact Us Message";
-    
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";  
+
     $contactus = "
-    You have a message from the contact us page on your website:
-    Name: ".$name."
-    Phone: ".$phone."
-    Email: ".$email."
-    Message: ".$message;
+    <html>
+    <p>You have a message from the contact us page on your website:</p>
+    <b>Name: </b>".$name."
+    <br><b>Phone: </b>".$phone."
+    <br><b>Email: </b>".$email."
+    <br><b>Message: </b>".$message."
+    </html>";
     $contactus  = wordwrap($contactus ,70);
-    $mail = mail($to_email,$subject,$contactus);
+    $mail = mail($to_email,$subject,$contactus,$headers);
     if (!$mail) {
       $res = print_r(error_get_last()['message']);
     } else {
