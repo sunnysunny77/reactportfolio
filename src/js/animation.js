6dc7f03ca71cd9ad1361eb6097f5aa040bb1ac75
daniel <contractor@lateralaspect.com.au>
2024-04-01 08:31:35 +0800

    up

--- src/js/animation.js
+++ src/js/animation.js
@@ -32,7 +32,7 @@ export const animation = () => {
     if (!cached) {
       setTimeout(() => {
         let scroll_pos = window.scrollY + window.innerHeight;
-        if (tempImg0 && stopbool && scroll_pos > getPosition(tempImg0) + tempImg0.offsetHeight) {
+        if (tempImg0 && stopbool && scroll_pos > getPosition(tempImg0) + tempImg0.offsetHeight + tempImg0.offsetHeight / 2) {
           stopbool = false;
           setTimeout(() => {
             tempImg0.classList.add("shrink");
