db107b6830bf9e3f7b77a612bbaf4878657535dc
daniel <contractor@lateralaspect.com.au>
2024-04-04 08:49:20 +0800

    Update animation.js

--- src/js/animation.js
+++ src/js/animation.js
@@ -22,6 +22,7 @@ export const animation = () => {
 
   const tempImg0 = document.querySelector(".template-img-0");
   const tempB = document.querySelector(".template-b");
+  const tempBorder = document.querySelector(".template-border-img")
   if (tempB) inner = tempB.innerHTML;
 
   let stopbool = true;
@@ -33,7 +34,7 @@ export const animation = () => {
       setTimeout(() => {
 
         let scroll_pos = window.scrollY + window.innerHeight;
-        if (tempImg0 && stopbool && scroll_pos > getPosition(tempImg0) + tempImg0.offsetHeight) {
+        if (tempImg0 && tempBorder && stopbool && scroll_pos > getPosition(tempBorder) + tempBorder.offsetHeight) {
           stopbool = false;
           setTimeout(() => {
             tempImg0.classList.add("shrink");
